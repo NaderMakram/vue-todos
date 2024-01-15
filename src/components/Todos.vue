0f57d1c8a441f29d3b77018d0f4c7afc2bdf1fe5
nader <nadermakram98@gmail.com>
2024-01-15 16:19:32 +0200

    first commit

---
+++ src/components/Todos.vue
@@ -0,0 +1,20 @@
+<script setup>
+import { useCounterStore } from '@/stores/counter'
+import { storeToRefs } from 'pinia'
+
+const store = useCounterStore()
+// `name` and `doubleCount` are reactive refs
+// This will also extract refs for properties added by plugins
+// but skip any action or non reactive (non ref/reactive) property
+const { name, doubleCount, count } = storeToRefs(store)
+// the increment action can just be destructured
+const { increment,fetchRecords } = store
+</script>
+
+<template>
+    <div id="app">
+      <h1>Counter: {{ count }}</h1>
+      <button @click="increment">Increment</button>
+      <button @click="fetchRecords">fetchRecords</button>
+    </div>
+  </template>
---
+++ src/stores/counter.js
@@ -0,0 +1,30 @@
+import { defineStore } from "pinia";
+import axios from "axios";
+
+export const useCounterStore = defineStore("counter", {
+  state: () => ({ count: 0, name: "Eduardo" }),
+  getters: {
+    doubleCount: (state) => state.count * 2,
+  },
+  actions: {
+    increment() {
+      this.count++;
+    },
+    async fetchRecords() {
+      try {
+        const response = await axios.get(
+          `https://api.airtable.com/v0/app4GVG5QETa94mUJ/tblgBZNGKlwS0RygH`,
+          {
+            headers: {
+              Authorization: `Bearer patR6qxROwtC6iFh6.73e5c0d694b44d0aebbdccd40e0acc44be65a899eff4c2e3a9408395756521ed`,
+            },
+          }
+        );
+        console.log(response);
+        this.records = response.data.records;
+      } catch (error) {
+        console.error("Error fetching Airtable records:", error);
+      }
+    },
+  },
+});
